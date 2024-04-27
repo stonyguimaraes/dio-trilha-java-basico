279982d73a106eb1554a50a8481db22a102e07df
stonyguimaraes <stonyvb@gmail.com>
2024-04-27 10:05:28 -0300

    variaveis basico

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+    }
+}
