b4a2cff38c2f233967a137b1127e7c96352a66f8
stonyguimaraes <stonyvb@gmail.com>
2024-05-03 09:25:10 -0300

    mensagem depois da condição for

--- estruturas-condicionais/src/ExemploFor.java
+++ estruturas-condicionais/src/ExemploFor.java
@@ -4,5 +4,7 @@ public class ExemploFor {
         for ( int carneirinhos = 0; carneirinhos <= 20; carneirinhos ++){
             System.out.println("Contando carneirinhos " + carneirinhos);
         }
+
+        System.out.println("Joaozinho dormiu ZzZz!!");
     }
 }
